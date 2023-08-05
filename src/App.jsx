1a2778183ffa9b6eb0751e918f2db89ef482d72f
maria haroon <mejoramiga.parasiempre@gmail.com>
2023-08-05 16:28:05 +0500

    begin

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='titlefont'>
+      My To-Do List
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/list.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function list() {
+    return (
+        <div>
+            <div>
+                <input type="button" value="" />
+                <button >
+                    ToDo
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default list
